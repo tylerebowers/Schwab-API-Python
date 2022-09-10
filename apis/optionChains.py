9482db40f120ca8e65ab8eb4187e35c3685072d4
Tyler Bowers <tylerebowers@gmail.com>
2022-09-10 02:11:36 -0400

    Update optionChains.py

--- apis/optionChains.py
+++ apis/optionChains.py
@@ -1,9 +1,16 @@
-# https://developer.tdameritrade.com/option-chains/apis
+"""
+APIs for Option Chains
+https://developer.tdameritrade.com/option-chains/apis
+"""
 import requests
 from variables import credentials
 
 
-def getStreamerSubscriptionKeys():  # NOT ADDED YET
+def getOptionChain(**kwargs):
+    args = ["symbol", "contractType", "strikeCount", "includeQuotes", "strategy", "interval", "strike", "range", "fromDate", "toDate", "volatility", "underlyingPrice", "interestRate", "daysToExpiration", "expMonth", "optionType"]
+    params = {}
+    for key, value in kwargs.items():
+        if key in args: params[key] = value
     return requests.get('https://api.tdameritrade.com/v1/marketdata/chains',
-                        params={},
+                        params=params,
                         headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
