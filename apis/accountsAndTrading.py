d7c56f9ac27af0701f9f8074ee9694e4e46a0053
Tyler Bowers <tylerebowers@gmail.com>
2022-09-10 02:09:52 -0400

    Update accountsAndTrading.py

--- apis/accountsAndTrading.py
+++ apis/accountsAndTrading.py
@@ -1,78 +1,139 @@
-import requests
+"""
+APIs for Accounts and Trading
+https://developer.tdameritrade.com/account-access/apis
+"""
+
+import requests, inspect
 from variables import credentials
 
-# ALL CODE HERE HAS NOT BEEN TESTED !! DO NOT USE
 
-#not tested
-def  cancelOrder(orderId): 
-    return requests.delete('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/orders/' + orderId,
+def cancelOrder(orderId):
+    return requests.delete('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/orders/' + orderId,
+                           headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def getOrder(orderId):
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/orders/' + orderId,
                         headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
-   
-#not tested   
-def getOrder(orderId):  
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/orders/' + orderId,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()  
 
-#not tested   
-def getOrdersByPath(maxResults, fromEnteredTime, toEnteredTime, status):  # FIX
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/orders/',
-                        params={'maxResults': maxResults, 'fromEnteredTime': fromEnteredTime, 'toEnteredTime': toEnteredTime, 'status': status},
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json() 
 
-#not tested   
-def getOrdersByQuery(accountId, maxResults, fromEnteredTime, toEnteredTime, status):  # FIX
+"""
+#Unfortunatley python doesn't support function overloading so if you prefer this method use this one.
+def getOrdersByPath(maxResults, fromEnteredTime, toEnteredTime, status):  # times are entered as "yyyy-MM-dd"
+    args = (inspect.signature(getOrdersByPath)).parameters.keys()
+    params = {}
+    for arg in args:
+        var = locals()[arg]
+        if var not in ["", "null", None]: params[arg] = var
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/orders',
+                        params=params,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+"""
+
+
+def getOrdersByPath(**kwargs):  # times are entered as "yyyy-MM-dd"
+    args = ["maxResults", "fromEnteredTime", "toEnteredTime", "status"]
+    params = {}
+    for key, value in kwargs.items():
+        if key in args: params[key] = value
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/orders',
+                        params=params,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+# options for status "AWAITING_PARENT_ORDER", "AWAITING_CONDITION", "AWAITING_MANUAL_REVIEW", "ACCEPTED", "AWAITING_UR_OUT", "PENDING_ACTIVATION", "QUEUED", "WORKING", "REJECTED", "PENDING_CANCEL", "CANCELED", "PENDING_REPLACE", "REPLACED", "FILLED", "EXPIRED"
+
+
+"""
+def getOrdersByQuery(accountId, maxResults, fromEnteredTime, toEnteredTime, status):
+    args = (inspect.signature(getOrdersByPath)).parameters.keys()
+    params = {}
+    for arg in args:
+        var = locals()[arg]
+        if var not in ["", "null", None]: params[arg] = var
     return requests.get('https://api.tdameritrade.com/v1/orders/',
-                        params={'accountId': accountId, 'maxResults': maxResults, 'fromEnteredTime': fromEnteredTime, 'toEnteredTime': toEnteredTime, 'status': status},
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json() 
+                        params=params,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+"""
 
-#not tested   
-def placeOrder(post):  # FIX
-    return requests.post('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/orders/',
-                        params=post,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json() 
 
-#not tested   
-def replaceOrder(put):  # FIX
-    return requests.put('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/orders/' + orderId,
-                        params=put,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json() 
-
-#not tested   
-def createSavedOrder(post):  # FIX
-    return requests.post('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/savedorders',
-                        params=post,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()                         
-                                              
-#not tested   
-def deleteSavedOrder(savedOrderId):  
-    return requests.delete('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/savedorders/' + savedOrderId,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()     
-                                                           
-#not tested   
-def getSavedOrder(savedOrderId):  
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/savedorders/' + savedOrderId,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()   
-
-#not tested   
-def getSavedOrdersByPath(): 
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/savedorders/',
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()                      
-                                   
-#not tested   
-def replaceSavedOrder(savedOrderId, put):  # FIX
-    return requests.put('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId + '/savedorders/' + savedOrderId,
-                        params=put,
+def getOrdersByQuery(**kwargs):
+    args = ["accountId", "maxResults", "fromEnteredTime", "toEnteredTime", "status"]
+    params = {}
+    for key, value in kwargs.items():
+        if key in args: params[key] = value
+    return requests.get('https://api.tdameritrade.com/v1/orders/',
+                        params=params,
                         headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
 
-#not tested   
-def getAccount(fields): 
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountId,
-                        params={'fields': fields},
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()  
 
-#not tested   
-def getAccounts(fields): 
+def placeOrder(data):
+    return requests.post('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/orders',
+                         data=data,
+                         headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def replaceOrder(orderId, data):
+    return requests.put('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/orders/' + orderId,
+                        data=data,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def createSavedOrder(data):  # FIX
+    return requests.post('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/savedorders',
+                         data=data,
+                         headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def deleteSavedOrder(savedOrderId):
+    return requests.delete('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/savedorders/' + savedOrderId,
+                           headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def getSavedOrder(savedOrderId):
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/savedorders/' + savedOrderId,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def getSavedOrdersByPath():
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/savedorders/',
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+# not tested
+def replaceSavedOrder(savedOrderId, data):  # FIX
+    return requests.put('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/savedorders/' + savedOrderId,
+                        params=data,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+# not tested
+def getAccount(**kwargs):
+    args = ["fields"]
+    params = {}
+    for key, value in kwargs.items():
+        if key in args: params[key] = value
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber,
+                        params=params,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+# not tested
+def getAccounts(**kwargs):
+    args = ["fields"]
+    params = {}
+    for key, value in kwargs.items():
+        if key in args: params[key] = value
     return requests.get('https://api.tdameritrade.com/v1/accounts/',
-                        params={'fields': fields},
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()  
-                         
+                        params=params,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def examples():
+    print("------------------------------------")
+    print("Examples for: api-accountsAndTrading")
+    print("------------------------------------")
+    print("No examples yet")
+    print("------------------------------------")
+    print("------------------------------------")
+    print("------------------------------------")
+    print("------------------------------------")
+    return
