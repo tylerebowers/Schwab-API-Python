c1d98fd4bf47cc9bd74303ee49535ca5919dec2b
Tyler Bowers <tylerebowers@gmail.com>
2022-09-10 02:12:59 -0400

    Update watchlist.py

--- apis/watchlist.py
+++ apis/watchlist.py
@@ -1,1 +1,68 @@
-#https://developer.tdameritrade.com/watchlist/apis
+"""
+APIs for Watchlist
+https://developer.tdameritrade.com/watchlist/apis
+"""
+
+import requests
+from variables import credentials
+
+
+# not tested
+def createWatchlist(data):  # FIX
+    return requests.post('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/watchlists',
+                         data=data,
+                         headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def deleteWatchlist(watchListId):  # FIX
+    return requests.delete(
+        'https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/watchlists/' + watchListId,
+        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def getWatchlist(watchListId):  # FIX
+    return requests.get(
+        'https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/watchlists/' + watchListId,
+        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def getWatchlistsForMultipleAccounts():  # FIX
+    return requests.get('https://api.tdameritrade.com/v1/accounts/watchlists',
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def getWatchlistsForSingleAccount():  # FIX
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/watchlists',
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def replaceWatchlist(watchListId, data):  # FIX
+    return requests.post('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/watchlists/' + watchListId,
+        data=data,
+        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+def updateWatchlist(watchListId, data):  # FIX
+    return requests.patch('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/watchlists/' + watchListId,
+        data=data,
+        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+
+'''
+# data for watchlists
+{
+    "name": "string",
+    "watchlistItems": [
+        {
+            "quantity": 0,
+            "averagePrice": 0,
+            "commission": 0,
+            "purchasedDate": "DateParam\"",
+            "instrument": {
+                "symbol": "string",
+                "assetType": "'EQUITY' or 'OPTION' or 'MUTUAL_FUND' or 'FIXED_INCOME' or 'INDEX'"
+            }
+        }
+    ]
+}
+'''
