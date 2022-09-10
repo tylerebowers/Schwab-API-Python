aad7f1c5f91c6fd3005362ea3e2368990e5979fc
Tyler Bowers <tylerebowers@gmail.com>
2022-09-10 02:10:12 -0400

    Update authentication.py

--- apis/authentication.py
+++ apis/authentication.py
@@ -1,34 +1,29 @@
+"""
+APIs for authentication
+https://developer.tdameritrade.com/authentication/apis
+"""
+
 from variables import credentials
 import requests
 
 debug = False
 
 
-def postAccessToken(grant_type, data):
+def postAccessToken(grant_type, code):
     if grant_type == 'authorization_code':
-        authCode = data
-        response = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
+        authCode = code
+        data = {'grant_type': 'authorization_code', 'access_type': 'offline', 'code': authCode,
+                'client_id': credentials.consumerKey, 'redirect_uri': credentials.callbackUrl}
+    elif grant_type == 'refresh_token':
+        refreshToken = code
+        data = {'grant_type': 'refresh_token', 'refresh_token': refreshToken,
+                'client_id': credentials.consumerKey}
+    else:
+        return "error"
+    response = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
                              headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                             data={'grant_type': 'authorization_code', 'access_type': 'offline', 'code': authCode,
-                                   'client_id': credentials.consumerKey, 'redirect_uri': credentials.callbackUrl})
-        if str(response) != "<Response [200]>":
-            print("Error with url, re-authenticate!")
-            quit()
-        return response.json()
-    if grant_type == 'refresh_token':
-        refreshToken = data
-        response = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
-                             headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                             data={'grant_type': 'refresh_token', 'refresh_token': refreshToken,
-                                   'client_id': credentials.consumerKey})
-        if str(response) != "<Response [200]>":
-            print("Error with url, re-authenticate!")
-            quit()
-        return response.json()
-
-
-
-
-
-
-
+                             data=data)
+    if str(response) != "<Response [200]>":
+        print("Error with url, re-authenticate!")
+        quit()
+    return response.json()
