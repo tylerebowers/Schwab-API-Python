90b28639ee0b2103663951c461caaabca2393eda
Tyler Bowers <tylerebowers@gmail.com>
2022-09-10 02:12:30 -0400

    Update transactionHistory.py

--- apis/transactionHistory.py
+++ apis/transactionHistory.py
@@ -1,13 +1,21 @@
-# https://developer.tdameritrade.com/transaction-history/apis
+"""
+APIs for Transaction History
+https://developer.tdameritrade.com/transaction-history/apis
+"""
 import requests
 from variables import credentials
 
 
-def getTransaction(transactionId):  # Working
+def getTransaction(transactionId):
     return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/transactions/' + transactionId,
-                            headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
 
-def getTransactions(type, ):  # NOT ADDED YET
-        return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/transactions',
-                                params={},
-                                headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+
+def getTransactions(**kwargs):
+    args = ["type", "symbol", "startDate", "endDate"]
+    params = {}
+    for key, value in kwargs.items():
+        if key in args: params[key] = value
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/transactions',
+                        params=params,
+                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
