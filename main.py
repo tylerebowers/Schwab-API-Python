365dde2b70f26124a55f1a46d81a2f41e861aa65
Tyler Bowers <tylerebowers@gmail.com>
2022-09-18 02:11:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-import asyncio
-from utilities import client
-from utilities import stream
-from apis import quotes
-from apis import priceHistory
+from apis import client, accountsAndTrading, authentication, instruments, marketHours, movers, optionChains, priceHistory, quotes, transactionHistory, userInfoAndPreferences, watchlist
+from stream import stream, levelOne
+from variables import globals
+from threading import Thread
+from time import sleep
 
-client.login()
-client.setupStream()
-print(quotes.getQuote('AMD'))
-#Steaming will be updated to make usage more logical in the near future (hint: threading)
-asyncio.run(stream.startStream())
 
+def main():  # these are some examples to get you started; remember to fill in globals.py
+    print(quotes.getQuote('AMD'))
+    print(priceHistory.getPriceHistory('AMD', 'day', '1', 'minute', '30', 'false'))
+    stream.send(levelOne.quoteRequest("AMD"))
+    sleep(30)
+    stream.send(stream.logoutRequest())
+
+
+if __name__ == '__main__':
+    client.login()
+    mainThread = Thread(target=main)
+    streamThread = Thread(target=stream.start)
+    streamThread.start()
+    sleep(2)
+    mainThread.start()
+    streamThread.join()
+    mainThread.join()
