43a00b84b930df815d13ccc599f533ebfb564d02
Tyler Bowers <tylerebowers@gmail.com>
2022-09-23 21:21:52 -0400

    Update authentication.py

--- apis/authentication.py
+++ apis/authentication.py
@@ -3,21 +3,19 @@ APIs for authentication
 https://developer.tdameritrade.com/authentication/apis
 """
 
-from variables import credentials
+from variables import globals
 import requests
-
-debug = False
 
 
 def postAccessToken(grant_type, code):
     if grant_type == 'authorization_code':
         authCode = code
         data = {'grant_type': 'authorization_code', 'access_type': 'offline', 'code': authCode,
-                'client_id': credentials.consumerKey, 'redirect_uri': credentials.callbackUrl}
+                'client_id': globals.consumerKey, 'redirect_uri': globals.callbackUrl}
     elif grant_type == 'refresh_token':
         refreshToken = code
         data = {'grant_type': 'refresh_token', 'refresh_token': refreshToken,
-                'client_id': credentials.consumerKey}
+                'client_id': globals.consumerKey}
     else:
         return "error"
     response = requests.post('https://api.tdameritrade.com/v1/oauth2/token',
