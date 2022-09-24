74a10f36b62a2d972866363a2d37eba852cb0d47
Tyler Bowers <tylerebowers@gmail.com>
2022-09-23 21:26:11 -0400

    Update transactionHistory.py

--- apis/transactionHistory.py
+++ apis/transactionHistory.py
@@ -3,12 +3,12 @@ APIs for Transaction History
 https://developer.tdameritrade.com/transaction-history/apis
 """
 import requests
-from variables import credentials
+from variables import globals
 
 
 def getTransaction(transactionId):
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/transactions/' + transactionId,
-        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + globals.accountNumber + '/transactions/' + transactionId,
+        headers={'Authorization': 'Bearer ' + globals.accessToken}).json()
 
 
 def getTransactions(**kwargs):
@@ -16,6 +16,6 @@ def getTransactions(**kwargs):
     params = {}
     for key, value in kwargs.items():
         if key in args: params[key] = value
-    return requests.get('https://api.tdameritrade.com/v1/accounts/' + credentials.accountNumber + '/transactions',
+    return requests.get('https://api.tdameritrade.com/v1/accounts/' + globals.accountNumber + '/transactions',
                         params=params,
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+                        headers={'Authorization': 'Bearer ' + globals.accessToken}).json()
