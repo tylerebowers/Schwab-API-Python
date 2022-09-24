746b4b7503d504da39d2e13b63edbb0bee73c780
Tyler Bowers <tylerebowers@gmail.com>
2022-09-23 21:18:46 -0400

    Update marketHours.py

--- apis/marketHours.py
+++ apis/marketHours.py
@@ -5,16 +5,16 @@ https://developer.tdameritrade.com/market-hours/apis
 
 
 import requests
-from variables import credentials
+from variables import globals
 
 
 def getHoursForMultipleMarkets(markets, date):  # date as "yyyy-MM-dd" OR "yyyy-MM-dd'T'HH:mm:ssz"
     return requests.get('https://api.tdameritrade.com/v1/marketdata/hours',
-                        params={'apikey': credentials.consumerKey, 'markets': markets, 'date': date},
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+                        params={'apikey': globals.consumerKey, 'markets': markets, 'date': date},
+                        headers={'Authorization': 'Bearer ' + globals.accessToken}).json()
 
 
 def getHoursForASingleMarkets(market, date):  # date as "yyyy-MM-dd" OR "yyyy-MM-dd'T'HH:mm:ssz"
     return requests.get('https://api.tdameritrade.com/v1/marketdata/' + market + '/hours',
-                        params={'apikey': credentials.consumerKey, 'date': date},
-                        headers={'Authorization': 'Bearer ' + credentials.accessToken}).json()
+                        params={'apikey': globals.consumerKey, 'date': date},
+                        headers={'Authorization': 'Bearer ' + globals.accessToken}).json()
