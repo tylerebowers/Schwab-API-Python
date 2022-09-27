c1e1f333ad1e12be533f5497ad40e71630269830
Tyler Bowers <tylerebowers@gmail.com>
2022-09-27 01:43:23 -0400

    Update levelOne.py

--- stream/levelOne.py
+++ stream/levelOne.py
@@ -6,17 +6,17 @@ from variables import globals
 def quoteRequest(ticker, fields):   # FIX
     globals.requestId += 1
     request = {
-        "requests": [basicRequest(service="QUOTE", command="SUBS", parameters={"keys": ticker, "fields": "0,3"})]}
+        "requests": [basicRequest(service="QUOTE", command="SUBS", parameters={"keys": ticker, "fields": fields)]}
     return json.dumps(request)
 
-
+#not complete do not use
 def optionRequest(ticker, fields):   # FIX
     globals.requestId += 1
     request = {
         "requests": [basicRequest(service="OPTION", command="SUBS", parameters={"keys": ticker, "fields": "0,3"})]}
     return json.dumps(request)
 
-
+#not complete do not use
 def levelOne_FuturesRequest(ticker, fields):   # FIX
     globals.requestId += 1
     request = {
