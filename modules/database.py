e3d6af1b4a949ec680a0a0a6756b0bc756840da1
Tyler Bowers <tylerebowers@gmail.com>
2022-10-18 18:31:07 -0400

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,12 +1,9 @@
+"""
 import psycopg2
 from modules import globals
 
 
 def initializeDatabase():
-    initDatabase()
-
-
-def initDatabase():
     globals.database = psycopg2.connect(
         host=globals.postgresql_host,
         user=globals.postgresql_username,
@@ -14,25 +11,4 @@ def initDatabase():
         database="stockbot"
     )
     dbCursor = globals.database.cursor()
-    dbCursor.execute("CREATE SCHEMA IF NOT EXISTS stocks AUTHORIZATION stockbot")
-    dbCursor.execute("CREATE SCHEMA IF NOT EXISTS options AUTHORIZATION stockbot")
-    dbCursor.execute("CREATE SCHEMA IF NOT EXISTS futures AUTHORIZATION stockbot")
-    dbCursor.execute("CREATE SCHEMA IF NOT EXISTS forex AUTHORIZATION stockbot")
-    globals.database.commit()
-
-
-def addStockToDatabase(ticker):
-    dbCursor = globals.database.cursor()
-    #dbCursor.execute("CREATE TABLE IF NOT EXISTS stock.%s", ticker)
-    return
-
-def testfunc():
-    dbCursor = globals.database.cursor()
-    dbCursor.execute("CREATE TABLE IF NOT EXISTS AMD (epoch VARCHAR(255), price FLOAT)")
-    sql = "INSERT INTO AMD (epoch, price) VALUES (%s, %s)"
-    val = (1001, None)
-    dbCursor.execute(sql, val)
-
-
-initializeDatabase()
-#addStockToDatabase("AMD")
+"""
