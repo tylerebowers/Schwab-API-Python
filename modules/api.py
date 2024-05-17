5b67d80016ffd3c6b06e87a2e7ec60219ff139bc
jrstrunk <developer@jrstrunk.com>
2024-05-16 20:34:49 -0400

    Fix initial authorization request By adding the client_id as part of the data, I was able to get a access and refresh token. Without it, the API would respond with a 400, saying "Bad Request"

--- modules/api.py
+++ modules/api.py
@@ -179,7 +179,8 @@ def _PostAccessTokenAutomated(grant_type, code):
         'Content-Type': 'application/x-www-form-urlencoded'}
     if grant_type == 'authorization_code':
         data = {'grant_type': 'authorization_code', 'code': code,
-                'redirect_uri': credentials.callbackUrl}  # gets access and refresh tokens using authorization code
+                'redirect_uri': credentials.callbackUrl, 
+                'client_id': credentials.appKey}  # gets access and refresh tokens using authorization code
     elif grant_type == 'refresh_token':
         data = {'grant_type': 'refresh_token', 'refresh_token': code}  # refreshes the access token
     else:
